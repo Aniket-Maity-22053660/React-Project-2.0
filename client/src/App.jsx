9475585aa2a2d4c4f7d4440a470d0ab71ead2e25
Aniket-Maity-22053660 <22053660@kiit.ac.in>
2025-07-06 12:37:20 +0000

    Committed!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import { Programs } from './components/Programs/Programs.jsx'
 import { Title } from './components/Title/Title.jsx'
 import { About } from './components/About/About.jsx'
 import { Campus } from './components/Campus/Campus.jsx'
+import { Testimonials } from './components/Testimonials/Testimonials.jsx'
 const App = ()=>{
   return(
     <>
@@ -17,6 +18,7 @@ const App = ()=>{
     <Title sub_heading='Gallery' heading='Campus Photos'/>
     <Campus />
     <Title sub_heading='testimonials' heading='What student says'/>
+    <Testimonials />
     </>
   )
 }
